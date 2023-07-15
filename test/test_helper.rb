ace113a711d5741306c844bfce092634c6050340
Patrick Schmid <prschmid@users.noreply.github.com>
2023-07-15 11:32:08 -0400

    Setup CircleCI

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,12 +2,6 @@
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 
-require 'test/unit'
-
 require 'minitest/autorun'
 require 'active_record'
 require 'unified_settings'
-
-ActiveRecord::Base.establish_connection(
-  adapter: 'sqlite3', database: ':memory:'
-)
